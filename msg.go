294367414ba4854481fa1ba4441716f795ba521b
faint <znetdragon@gmail.com>
2014-10-11 10:49:28 +0800

    增加不限制大小的解包函数

--- msg.go
+++ msg.go
@@ -53,6 +53,31 @@ func UnPack(b []byte) (Msg, error) {
 	return m, nil
 }
 
+// 解包，不限制大小（用于消息存储
+func UnPackUnLimited(b []byte) (Msg, error) {
+	m := Msg{}
+	buf := bytes.NewBuffer(b)
+	// 消息类型
+	mType := buf.Next(SIZE_OF_TYPE)
+	bufType := bytes.NewBuffer(mType)
+	binary.Read(bufType, binary.LittleEndian, &m.Type)
+	// 消息大小
+	mSize := buf.Next(SIZE_OF_SIZE)
+	bufSize := bytes.NewBuffer(mSize)
+	binary.Read(bufSize, binary.LittleEndian, &m.Size)
+	// // 超限则返回错误
+	// if m.Size > MAX_BUFFER {
+	// 	return m, errors.New("OVER_MAX_BUFFER")
+	// }
+	// 消息正文
+	mContent := buf.Bytes()
+	rest := int(m.Size - int32(SIZE_OF_HEAD))
+	if rest > 0 {
+		m.Content = mContent[:rest]
+	}
+	return m, nil
+}
+
 // 打包消息，返回[]byte
 func Pack(mType int32, mContent []byte) []byte {
 	buf := new(bytes.Buffer)
