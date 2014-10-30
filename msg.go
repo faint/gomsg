6e82bf9a06c697c8bd2927c1af49907332e6ad96
xiaoweiwei <chronicle2nd_ww@hotmail.com>
2014-10-30 10:48:00 +0800

    增加了MsgBig

--- msg.go
+++ msg.go
@@ -21,11 +21,22 @@ const (
 	SIZE_OF_HEAD = SIZE_OF_TYPE + SIZE_OF_SIZE
 )
 
+const (
+	MAX_BUFFER_BIG   = 2147483647
+	SIZE_OF_SIZE_BIG = 8
+)
+
 // 消息的结构
 type Msg struct {
 	Type    int32  // 消息类型
 	Size    int32  // 消息大小（含消息类型和消息大小自身
 	Content []byte // 消息正文
+}
+
+// 用以存储的消息结构
+type MsgBig struct {
+	Size    int32  //消息大小
+	Content []byte //消息正文
 }
 
 // 解包消息，返回Msg类型
@@ -58,33 +69,33 @@ func UnPack(b []byte) (Msg, error) {
 }
 
 // 解包，不限制大小（用于消息存储
-func UnPackUnLimited(b []byte) (Msg, error) {
-	m := Msg{}
-	buf := bytes.NewBuffer(b)
-	// 消息类型
-	mType := buf.Next(SIZE_OF_TYPE)
-	bufType := bytes.NewBuffer(mType)
-	binary.Read(bufType, binary.LittleEndian, &m.Type)
-	// 消息大小
-	mSize := buf.Next(SIZE_OF_SIZE)
-	bufSize := bytes.NewBuffer(mSize)
-	binary.Read(bufSize, binary.LittleEndian, &m.Size)
-	// // 超限则返回错误
-	// if m.Size > MAX_BUFFER {
-	// 	return m, errors.New("OVER_MAX_BUFFER")
-	// }
-	// 消息正文
-	mContent := buf.Bytes()
-	rest := int(m.Size - int32(SIZE_OF_HEAD))
-	if rest > 0 {
-		if rest > len(mContent)-1 {
-			m.Content = mContent
-		} else {
-			m.Content = mContent[:rest]
-		}
-	}
-	return m, nil
-}
+// func UnPackUnLimited(b []byte) (Msg, error) {
+// 	m := Msg{}
+// 	buf := bytes.NewBuffer(b)
+// 	// 消息类型
+// 	mType := buf.Next(SIZE_OF_TYPE)
+// 	bufType := bytes.NewBuffer(mType)
+// 	binary.Read(bufType, binary.LittleEndian, &m.Type)
+// 	// 消息大小
+// 	mSize := buf.Next(SIZE_OF_SIZE)
+// 	bufSize := bytes.NewBuffer(mSize)
+// 	binary.Read(bufSize, binary.LittleEndian, &m.Size)
+// 	// // 超限则返回错误
+// 	// if m.Size > MAX_BUFFER {
+// 	// 	return m, errors.New("OVER_MAX_BUFFER")
+// 	// }
+// 	// 消息正文
+// 	mContent := buf.Bytes()
+// 	rest := int(m.Size - int32(SIZE_OF_HEAD))
+// 	if rest > 0 {
+// 		if rest > len(mContent)-1 {
+// 			m.Content = mContent
+// 		} else {
+// 			m.Content = mContent[:rest]
+// 		}
+// 	}
+// 	return m, nil
+// }
 
 // 打包消息，返回[]byte
 func Pack(mType int32, mContent []byte) []byte {
@@ -185,4 +196,39 @@ func CopyBytes(a, b []byte) []byte {
 	copy(result, a)
 	copy(result[n:], b)
 	return result
+}
+
+func PackMsgBig(mContent []byte) []byte {
+	buf := new(bytes.Buffer)
+	// 消息大小
+	mSize := int32(SIZE_OF_SIZE_BIG + len(mContent))
+	binary.Write(buf, binary.LittleEndian, mSize)
+	// 消息正文
+	binary.Write(buf, binary.LittleEndian, mContent)
+	b := buf.Bytes()
+	return b
+}
+
+func UnpackMsgBig(b []byte) (MsgBig, error) {
+	m := MsgBig{}
+	buf := bytes.NewBuffer(b)
+	// 消息大小
+	mSize := buf.Next(SIZE_OF_SIZE_BIG)
+	bufSize := bytes.NewBuffer(mSize)
+	binary.Read(bufSize, binary.LittleEndian, &m.Size)
+	// 超限则返回错误
+	if m.Size > MAX_BUFFER_BIG {
+		return m, errors.New("OVER_MAX_BUFFER_BIG")
+	}
+	// 消息正文
+	mContent := buf.Bytes()
+	rest := int(m.Size - int32(SIZE_OF_SIZE_BIG))
+	if rest > 0 {
+		if rest > len(mContent)-1 {
+			m.Content = mContent
+		} else {
+			m.Content = mContent[:rest]
+		}
+	}
+	return m, nil
 }
